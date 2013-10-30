73ef8a5ac5e0ce77c5b0caf4c15e58f2c5a798d3
Daniel Strabley <strabley@gmail.com>
2013-10-30 16:13:40 -0500

    adding first verison of hobo app

---
+++ app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+App::Application.config.secret_token = '0e6aac927794bf96a994064618a95336228070e52d52027b9019085b1b6027893cb30ea8cb950080a27aa4c7bf731340d9c707cb6b73aef26b536122deb47acb'
